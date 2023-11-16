b8b9cacae4738496992d4407493a9abc504158f7
Tito Sopena <tito.sopena@gmail.com>
2023-11-16 17:01:12 -0600

    finishes static mobile menu and starts Login forms

--- TPD/src/components/NavBar/NavBar.jsx
+++ TPD/src/components/NavBar/NavBar.jsx
@@ -1,12 +1,27 @@
+import { useState } from 'react'
+
 const NavBar = () => {
+  //Brings in useState hook to keep track if menu is open
+  const [isMenuOpen, setMenuOpen] = useState(false);
+
+  //event handler to toggle menu open and close
+  const toggleMenu = () => {
+    setMenuOpen(!isMenuOpen);
+  }
+
   return (
     <nav className="navbar">
-      <div className="logo">Logo</div>
-      <div className={`menu-icon`}>
-        <div>Link 1</div>
-        <div>Link 2</div>
-        <div>Link 3</div>
+      <div className="logo">Your Logo</div>
+      <div className={`menu ${isMenuOpen ? 'open' : ''}`} onClick={toggleMenu}>
+        <div className="line"></div>
+        <div className="line"></div>
+        <div className="line"></div>
       </div>
+      <ul className={`nav-link ${isMenuOpen ? 'open' : ''}`}>
+        <li>Home</li>
+        <li>About</li>
+        <li>Contact</li>
+      </ul>
     </nav>
   );
 };
